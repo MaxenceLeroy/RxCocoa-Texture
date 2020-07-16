fced3ca84709262c91393bb97116625ad2d8ee97
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2020-07-16 16:31:04 +0200

    Add carthage support

---
+++ RxCocoa_Texture/RxCocoa_Texture.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxCocoa_Texture.
+FOUNDATION_EXPORT double RxCocoa_TextureVersionNumber;
+
+//! Project version string for RxCocoa_Texture.
+FOUNDATION_EXPORT const unsigned char RxCocoa_TextureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxCocoa_Texture/PublicHeader.h>
+
+
